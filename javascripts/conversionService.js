6ab027329695b2c995ca1e15a87a023eabf4687f
Ricky Johnson <ricky.johnson@techsoft3d.com>
2022-11-02 09:17:55 -0400

    initial commit

---
+++ javascripts/conversionService.js
@@ -0,0 +1,45 @@
+modelUIDs = [
+        "1be61fc1-b6ab-4a06-812f-d1f225ee179d","aa7377e6-4cfc-4915-aebd-651df0e62038","f204aac5-ac1b-440a-ba5d-85f20f9ac076",
+        "f9781092-7efd-4d0e-a897-d4daf8b412e1",
+        "47478491-0e47-4270-9590-ffc7164df2b3",
+        "f5b9c8bb-a310-4ab0-b405-ace25ed890b5" ,
+        "1d19abf3-c06d-44ea-a9c3-9ac989bd6a22",
+        "8e80c44c-7593-4d99-b110-3c14749bc5b3",
+        "c6ae6232-a2f8-4f4f-801b-b0a2e4b96825",
+        "47a469cf-2cd0-4206-b6db-777f63956e4a",
+        "454372dc-2605-4143-917c-43c12c1fc36d",
+        "230ce4e4-103e-42f0-9b7d-b8d3d1d6faf4",
+        "6db017dd-e37b-480a-8d29-3cbd33be5019",
+        "0d5a3249-7e31-458b-89a5-8037379fe6f7",
+        "00f31b86-d8fa-48b3-80cd-d4cc19faf6ce",
+        "a076e40b-2935-4005-9d29-90bb716f32d2",
+        "337cedf7-c547-4719-bfc0-4833da0c254e",
+        "6d245164-aaef-4339-9246-1fe2127cc6a3",
+        "17828854-c048-4c84-b9b2-21058647dcb8",
+]
+
+async function startViewer(modelName, uid) {
+        const conversionServiceURI = "https://csapi.techsoft3d.com";
+
+        var viewer;
+
+        let res = await fetch(conversionServiceURI + '/api/streamingSession');
+        var data = await res.json();
+
+        await fetch(conversionServiceURI + '/api/enableStreamAccess/' + data.sessionid, { method: 'put', headers: { 'items': JSON.stringify(modelUIDs) } });
+
+        viewer = new Communicator.WebViewer({
+                containerId: "viewerContainer",
+                endpointUri: 'wss://' + data.serverurl + ":" + data.port + '?token=' + data.sessionid,
+                model: modelName,
+                streamingMode: Communicator.StreamingMode.OnDemand,
+                boundingPreviewMode: Communicator.BoundingPreviewMode.All,
+                enginePath: "https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@2022.2",
+                rendererType: 0
+        });
+
+        viewer.start();
+
+        return [viewer, data];
+
+}
---
+++ javascripts/custom.js
@@ -0,0 +1,569 @@
+cameras = [
+    //cement plant
+    {
+        "position": {
+            "x": 157909.9150886354,
+            "y": -336596.1606558162,
+            "z": 72360.0932463801
+        },
+        "target": {
+            "x": 456052.65833515854,
+            "y": -32931.74280371948,
+            "z": -37687.12353626976
+        },
+        "up": {
+            "x": 0.1775707758980264,
+            "y": 0.17651271312757597,
+            "z": 0.968148687780609
+        },
+        "width": 439558.37375896005,
+        "height": 439558.37375896005,
+        "projection": 1,
+        "nearLimit": 0.01,
+        "className": "Communicator.Camera"
+    },
+//control-feed-silo
+{
+    "position": {
+        "x": 418085.9280201621,
+        "y": 432143.406758749,
+        "z": 136939.85265403584
+    },
+    "target": {
+        "x": 438371.0414643172,
+        "y": 280897.125081398,
+        "z": 75684.10157572349
+    },
+    "up": {
+        "x": 0.03663129819300606,
+        "y": -0.37091116921269646,
+        "z": 0.9279456086139779
+    },
+    "width": 164435.97717298666,
+    "height": 164435.97717298666,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+ //cooler
+ {
+    "position": {
+        "x": 326857.03072219074,
+        "y": 312902.1923470424,
+        "z": 43829.54129864751
+    },
+    "target": {
+        "x": 326280.4705194033,
+        "y": 253785.2209943839,
+        "z": 24049.53926523131
+    },
+    "up": {
+        "x": 0.003921131907840007,
+        "y": -0.3173328597585353,
+        "z": 0.9483061113596344
+    },
+    "width": 62340.975321376085,
+    "height": 62340.975321376085,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+
+ //pan conveyor
+ {
+    "position": {
+        "x": 207389.62083094104,
+        "y": 102748.88265335641,
+        "z": 47695.36924055706
+    },
+    "target": {
+        "x": 284018.8646516396,
+        "y": 176671.38713666698,
+        "z": 26995.580886168253
+    },
+    "up": {
+        "x": 0.1353853997796659,
+        "y": 0.134529928078027,
+        "z": 0.9816172838626167
+    },
+    "width": 108466.85629967166,
+    "height": 108466.85629967166,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+//mill hopper
+{
+    "position": {
+        "x": 255277.38266893185,
+        "y": -46857.36215821363,
+        "z": 13844.066930118715
+    },
+    "target": {
+        "x": 311724.9053001333,
+        "y": -5177.657371408193,
+        "z": 7549.286001107841
+    },
+    "up": {
+        "x": 0.07295493113614311,
+        "y": 0.051619064208210656,
+        "z": 0.9959985191922673
+    },
+    "width": 70449.59098007233,
+    "height": 70449.59098007233,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+//mill-dept
+{
+    "position": {
+        "x": 340570.66155742673,
+        "y": 33541.42732637179,
+        "z": 39995.3259063109
+    },
+    "target": {
+        "x": 381932.05221352313,
+        "y": 77200.8836823589,
+        "z": 22753.533815489485
+    },
+    "up": {
+        "x": 0.19262399569678315,
+        "y": 0.1971341184028133,
+        "z": 0.9612669429681598
+    },
+    "width": 62563.50502337342,
+    "height": 62563.50502337342,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+    //pozzolan
+    {
+        "position": {
+            "x": 499729.9389429954,
+            "y": -299618.14102257974,
+            "z": 23313.020470793206
+        },
+        "target": {
+            "x": 439895.8539355046,
+            "y": -240832.06606462572,
+            "z": 5483.928280292828
+        },
+        "up": {
+            "x": -0.14787080901191824,
+            "y": 0.14615324785152464,
+            "z": 0.9781479703933408
+        },
+        "width": 85754.28191048645,
+        "height": 85754.28191048645,
+        "projection": 1,
+        "nearLimit": 0.01,
+        "className": "Communicator.Camera"
+    },
+
+
+ //coal-stacker
+ {
+    "position": {
+        "x": 260765.70035385227,
+        "y": -278729.33424009715,
+        "z": 26325.23108617577
+    },
+    "target": {
+        "x": 209662.56815766188,
+        "y": -222889.3838703704,
+        "z": 11618.393898484985
+    },
+    "up": {
+        "x": -0.13235700806673695,
+        "y": 0.13740770693451368,
+        "z": 0.9816316745554927
+    },
+    "width": 77109.79988056055,
+    "height": 77109.79988056055,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+ //esp
+ {
+    "position": {
+        "x": 556222.1218395805,
+        "y": 54514.67269518129,
+        "z": 101770.07270977723
+    },
+    "target": {
+        "x": 430997.7256500981,
+        "y": 175341.5201292212,
+        "z": 64790.033715136975
+    },
+    "up": {
+        "x": -0.14685187125047905,
+        "y": 0.14717367999208156,
+        "z": 0.9781484733044474
+    },
+    "width": 177898.28483128845,
+    "height": 177898.28483128845,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+//cement-silos
+{
+    "position": {
+        "x": 651239.9580776928,
+        "y": 517.8652910442438,
+        "z": 41951.72737740725
+    },
+    "target": {
+        "x": 621479.2055996311,
+        "y": 57544.80258057361,
+        "z": 29433.785138845466
+    },
+    "up": {
+        "x": -0.07946267135726748,
+        "y": 0.17398707658071505,
+        "z": 0.9815366427411987
+    },
+    "width": 65532.227511144,
+    "height": 65532.227511144,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+//packing plant
+{
+    "position": {
+        "x": 679237.3804588988,
+        "y": 69440.91952174579,
+        "z": 24685.062599284305
+    },
+    "target": {
+        "x": 642286.1049028403,
+        "y": 68149.8614979427,
+        "z": 15511.078063689696
+    },
+    "up": {
+        "x": -0.24068122130711264,
+        "y": -0.008145562403722918,
+        "z": 0.9705700384429987
+    },
+    "width": 38094.95489300267,
+    "height": 38094.95489300267,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+//truck loading
+{
+    "position": {
+        "x": 727890.2323770861,
+        "y": 17369.930147604136,
+        "z": 9956.793642424052
+    },
+    "target": {
+        "x": 704851.8665665733,
+        "y": 38835.21033100903,
+        "z": 4970.378855155013
+    },
+    "up": {
+        "x": -0.11165233263447838,
+        "y": 0.10960567429713533,
+        "z": 0.9876843386321108
+    },
+    "width": 31880.854521197747,
+    "height": 31880.854521197747,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+   //wagon loading
+   {
+    "position": {
+        "x": 829493.339896315,
+        "y": 87801.3062941505,
+        "z": 10502.697353863752
+    },
+    "target": {
+        "x": 797594.7076137911,
+        "y": 55902.67401190247,
+        "z": 5761.289915463384
+    },
+    "up": {
+        "x": -0.07391278508715228,
+        "y": -0.07391278531998292,
+        "z": 0.9945218953682733
+    },
+    "width": 45359.965029425126,
+    "height": 45359.965029425126,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+//wagon tippler 
+{
+    "position": {
+        "x": 785160.5960862736,
+        "y": -376683.4672148817,
+        "z": 56845.809090573646
+    },
+    "target": {
+        "x": 723609.9737989958,
+        "y": -277286.50978834776,
+        "z": 19741.785296700888
+    },
+    "up": {
+        "x": -0.07262338321486703,
+        "y": 0.30901365902874306,
+        "z": 0.9482807615596208
+    },
+    "width": 122657.82825119478,
+    "height": 122657.82825119478,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+//control-room
+{
+    "position": {
+        "x": 381214.40234375,
+        "y": 79399.40625,
+        "z": 1565113.9053525727
+    },
+    "target": {
+        "x": 381214.40234375,
+        "y": 79399.40625,
+        "z": -40.24560546875
+    },
+    "up": {
+        "x": 0,
+        "y": 1,
+        "z": 0
+    },
+    "width": 1565154.1509580377,
+    "height": 1565154.1509580377,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+  //cyclone-preheater
+  {
+    "position": {
+        "x": 279975.6199722725,
+        "y": 223855.0066163474,
+        "z": 93324.77273613974
+    },
+    "target": {
+        "x": 435424.8204907806,
+        "y": 225221.61341427418,
+        "z": 67310.81352635268
+    },
+    "up": {
+        "x": 0.16504330133655853,
+        "y": 0.0009878718531247431,
+        "z": 0.9862858271277815
+    },
+    "width": 157616.7745823773,
+    "height": 157616.7745823773,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+  //kiln-hot-air-duct
+  {
+    "position": {
+        "x": 368603.3326314076,
+        "y": 271754.69320811704,
+        "z": 19952.08903340799
+    },
+    "target": {
+        "x": 383543.3834746798,
+        "y": 225716.55551452911,
+        "z": 14005.22278958421
+    },
+    "up": {
+        "x": 0.04091836421629511,
+        "y": -0.1149291543803626,
+        "z": 0.9925305924470428
+    },
+    "width": 48765.56633132917,
+    "height": 48765.56633132917,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+//limestone-stockpile
+{
+    "position": {
+        "x": 590528.3260109734,
+        "y": 568403.3747809175,
+        "z": 9934.354576410107
+    },
+    "target": {
+        "x": 647525.1738022978,
+        "y": 505046.43219431466,
+        "z": -2033.3804856214729
+    },
+    "up": {
+        "x": 0.09660135118265638,
+        "y": -0.10015216425241034,
+        "z": 0.9902715400056913
+    },
+    "width": 86057.94277453114,
+    "height": 86057.94277453114,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+//raw mill hopper
+{
+    "position": {
+        "x": 599424.146003419,
+        "y": 216210.42967905384,
+        "z": 36438.91771474262
+    },
+    "target": {
+        "x": 479089.93701794563,
+        "y": 140315.25376511016,
+        "z": 8371.307397368648
+    },
+    "up": {
+        "x": -0.1768326555241416,
+        "y": -0.08234285987288618,
+        "z": 0.9807904288726693
+    },
+    "width": 145011.0007140699,
+    "height": 145011.0007140699,
+    "projection": 1,
+    "nearLimit": 0.01,
+    "className": "Communicator.Camera"
+},
+
+    //raw mill-ducting
+    {
+        "position": {
+            "x": 556429.627455032,
+            "y": 298844.20430986007,
+            "z": 40172.384865953165
+        },
+        "target": {
+            "x": 486698.8240129633,
+            "y": 254864.7604054644,
+            "z": 23907.87426144455
+        },
+        "up": {
+            "x": -0.1768326555241417,
+            "y": -0.08234285987288618,
+            "z": 0.9807904288726693
+        },
+        "width": 84030.41556496364,
+        "height": 84030.41556496364,
+        "projection": 1,
+        "nearLimit": 0.01,
+        "className": "Communicator.Camera"
+    },
+   
+
+];
+
+function setView(val) {
+    if (val >= 0) {
+        var camera = Communicator.Camera.construct(cameras[val]);
+        viewer.getView().setCamera(camera, 500);
+    }
+}
+
+function snapToView(val) {
+    if (val >= 0) {
+        var camera = Communicator.Camera.construct(cameras[val]);
+        viewer.getView().setCamera(camera, 500); // about as fast as this can go
+    }
+}
+
+var enabledNodes = []
+
+/*
+  This function either changes the buttons as the user requests different nodes, or changes the camera view as a user selects different
+  buttons. If the user enables the entire cement plant,then all of the other buttons are changed to view buttons. However, if the user 
+  clicks on just one node, then the buttons for the other nodes remain as request buttons. These node buttons only turn into view buttons 
+  when the user clicks to request the corresponding node.  This is because once the node is requested, it cannot be unloaded. 
+  So if the node has already been requested, when the user clicks on the button again the camera will zoom to the node location .  
+*/
+function requestOrViewNode(node){
+    if(node == 1){
+        if(enabledNodes.length != 19){
+            viewer.getModel().requestNodes([1]);
+            toggleEnableBtns("turn all to view");
+            enabledNodes = [].concat([2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38])
+        }
+        else {
+            setView(0)
+        }
+
+    }
+    else if (!enabledNodes.includes(node)){
+        enabledNodes.push(node);
+        viewer.getModel().requestNodes(enabledNodes);
+
+        //turn the most recently pressed button to view
+        var buttonToDisable = "enableBtn".concat(node/2)  //divide by 2 because the node indexes increase by 2, however their corresponding button name increase by 1
+        document.getElementById(buttonToDisable).childNodes[0].nodeValue = 'View'
+    }
+    else{
+        setView(node/2) //divide by 2 because the node indexes increase by 2, however their corresponding views increase by 1       
+        viewer.selectionManager.selectNode(node)
+                
+    }
+
+}
+
+function toggleEnableBtns(btnState){
+    if(btnState == "turn all to view"){
+        for(let i = 0; i < 20; i++ ){
+            var elementString = "enableBtn".concat(i)
+            document.getElementById(elementString).childNodes[0].nodeValue = 'View'
+        }
+    }
+}
+
+
+// function showLoadingPopup(){
+//     $.notify.addStyle('mystyle', {
+//         html: "<div><span data-notify-text/></div>",
+//         classes: {
+//         base: {
+//             "white-space": "nowrap",
+//             "background-color": "#0090d0",
+//             "padding": "5px",
+//             'color': 'white',
+//             'box-shadow': "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+//             'border-radius': "25px",
+//             'padding': "15px"
+//         }
+//         }
+//     });
+//     $.notify(
+//         `Model structure loading. This\ndialog will disappear when the\nscene can be interacted with. \n`,
+//         {
+//         globalPosition:"bottom right",
+//         className:'info',
+//         style:'mystyle',
+//         autoHide: false
+//     });
+// }
+
+// function closeLoadingPopup(){
+//     $('.notifyjs-wrapper').trigger('notify-hide');
+// }
