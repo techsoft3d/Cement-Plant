4638809f2b57a7b1908b3efcee974bef4a43b38e
Ricky Johnson <ricky.johnson@techsoft3d.com>
2024-03-20 14:31:35 -0400

    upgrade to 2024

--- javascripts/conversionService.js
+++ javascripts/conversionService.js
@@ -31,7 +31,7 @@ async function startViewer(modelName) {
                 model: modelName,
                 streamingMode: Communicator.StreamingMode.OnDemand,
                 boundingPreviewMode: Communicator.BoundingPreviewMode.All,
-                enginePath: "https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer",
+                enginePath: `https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@20${versionNumer}`,
                 rendererType: 0
         });
 
